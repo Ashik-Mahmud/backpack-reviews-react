6a1bea778ab82817009a551903f196e5aa1f42d1
ashikmahmud <ashikmamud187@gmail.com>
2022-04-04 01:38:47 +0600

    yeah done my header area with menus options

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
     <header id="header">
-      <div className="container">Header</div>
+      <nav>
+        <ul>
+          {["home", "reviews", "dashboard", "blogs"].map((link, ind) => (
+            <MenuLinks key={link + ind} name={link} />
+          ))}
+        </ul>
+      </nav>
     </header>
+  );
+};
+
+const MenuLinks = ({ name }) => {
+  return (
+    <li>
+      <NavLink to={name === "home" ? "/" : "/" + name}>{name}</NavLink>
+    </li>
   );
 };
 
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.css";
+const NotFound = () => {
+  return (
+    <section id="not-found">
+      <div className="container">
+        <h1>NotFound</h1>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
