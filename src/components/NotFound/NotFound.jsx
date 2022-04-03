64aacdd0922afedfe3a4c8af1a588e8723fe9f70
ashikmahmud <ashikmamud187@gmail.com>
2022-04-04 01:49:58 +0600

    done my notFound page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import "./NotFound.css";
+import { useNavigate } from "react-router-dom";
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <section id="not-found">
+    <section
+      onClick={() => navigate("/")}
+      id="not-found"
+      style={{ textAlign: "center", cursor: "pointer" }}
+    >
       <div className="container">
-        <h1>NotFound</h1>
+        <img
+          src="https://media.istockphoto.com/vectors/error-page-or-file-not-found-icon-vector-id924949200?k=20&m=924949200&s=612x612&w=0&h=n3NzIx5MWfdouq0iyYiegzPrOfR6J4B-luYlIsAM-2E="
+          alt=""
+        />
       </div>
     </section>
   );
