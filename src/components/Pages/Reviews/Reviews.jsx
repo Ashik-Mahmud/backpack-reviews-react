459d01ed846ed4ab3009a82441df62842cf2d410
ashikmahmud <ashikmamud187@gmail.com>
2022-04-04 03:10:15 +0600

    fixes problem of deploy issues on netlify with react-router

--- src/components/Pages/Reviews/Reviews.jsx
+++ src/components/Pages/Reviews/Reviews.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import Review from "./../../Review/Review";
 import "./Reviews.css";
 const Reviews = () => {
   return (
     <section id="reviews">
-      <div className="container">Reviews</div>
+      <div className="container">
+        <div className="review-title">
+          <h2>All the Customer Reviews</h2>
+          <p>Get all the reviews which one say people about my product</p>
+        </div>
+        <div className="reviews-content">
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+          <Review />
+        </div>
+      </div>
     </section>
   );
 };
