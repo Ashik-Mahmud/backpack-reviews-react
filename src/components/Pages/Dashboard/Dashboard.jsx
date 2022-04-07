62f7d263abd936c86e579f13bff759329b0f38c2
ashikmahmud <ashikmamud187@gmail.com>
2022-04-08 03:04:04 +0600

    responsive chart for dashboard

--- src/components/Pages/Dashboard/Dashboard.jsx
+++ src/components/Pages/Dashboard/Dashboard.jsx
@@ -8,6 +8,7 @@ import {
   Legend,
   Line,
   LineChart,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -28,90 +29,101 @@ const Dashboard = () => {
         <div className="chart-row">
           <div className="chart-col">
             <h3 className="graph-title">Investment Analytics</h3>
-            <AreaChart
-              width={630}
-              height={250}
-              data={data}
-              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-            >
-              <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-                </linearGradient>
-                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <XAxis dataKey="month" />
-              <YAxis />
-              <CartesianGrid strokeDasharray="3 3" />
-              <Tooltip />
-              <Area
-                type="monotone"
-                dataKey="sell"
-                stroke="#8884d8"
-                fillOpacity={1}
-                fill="url(#colorUv)"
-              />
-              <Area
-                type="monotone"
-                dataKey="revenue"
-                stroke="#82ca9d"
-                fillOpacity={1}
-                fill="url(#colorPv)"
-              />
-              <Area
-                type="monotone"
-                dataKey="sell"
-                stroke="#62d2a2"
-                fillOpacity={1}
-                fill="url(#colorPv)"
-              />
-            </AreaChart>
+            <div style={{ width: 600, height: 300 }}>
+              <ResponsiveContainer>
+                <AreaChart
+                  width={630}
+                  height={250}
+                  data={data}
+                  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+                >
+                  <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                      <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                    </linearGradient>
+                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                  <XAxis dataKey="month" />
+                  <YAxis />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip />
+                  <Area
+                    type="monotone"
+                    dataKey="sell"
+                    stroke="#8884d8"
+                    fillOpacity={1}
+                    fill="url(#colorUv)"
+                  />
+                  <Area
+                    type="monotone"
+                    dataKey="revenue"
+                    stroke="#82ca9d"
+                    fillOpacity={1}
+                    fill="url(#colorPv)"
+                  />
+                  <Area
+                    type="monotone"
+                    dataKey="sell"
+                    stroke="#62d2a2"
+                    fillOpacity={1}
+                    fill="url(#colorPv)"
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            </div>
           </div>
           <div className="chart-col">
             <h3 className="graph-title">Sell Analytics</h3>
-
-            <BarChart width={630} height={250} data={data}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="month" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="investment" fill="#8884d8" />
-              <Bar dataKey="revenue" fill="#82ca9d" />
-              <Bar dataKey="sell" fill="#62d2a2" />
-            </BarChart>
+            <div style={{ width: 600, height: 300 }}>
+              <ResponsiveContainer>
+                <BarChart width={630} height={250} data={data}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="month" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="investment" fill="#8884d8" />
+                  <Bar dataKey="revenue" fill="#82ca9d" />
+                  <Bar dataKey="sell" fill="#62d2a2" />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
           </div>
           <div className="chart-col span">
             <h3 className="graph-title">Revenue Analytics</h3>
-            <LineChart
-              width={1270}
-              height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="month" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="investment"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-              <Line type="monotone" dataKey="sell" stroke="#62d2a2" />
-              <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
-            </LineChart>
+            <div className="tall-chart" style={{ height: 300 }}>
+              <ResponsiveContainer>
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={data}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="month" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="investment"
+                    stroke="#8884d8"
+                    activeDot={{ r: 8 }}
+                  />
+                  <Line type="monotone" dataKey="sell" stroke="#62d2a2" />
+                  <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
           </div>
         </div>
       </div>
